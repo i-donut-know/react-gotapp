d1fae7c7d408dc33c20515a763b014f6c143e439
Kseniya <mylittleuniverse@list.ru>
2021-06-09 01:07:53 +0700

    Работа с API, обработка ошибок, лоадинга

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-eclipse-x17xfiddefe">
+            <div className="ldio-mx6b5bpp87">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
